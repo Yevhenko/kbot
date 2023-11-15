625b830e3cf31927a81a2c03bd829ed6283e89ae
Yevhen Svyrydov <55544104+Yevhenko@users.noreply.github.com>
2023-11-15 10:08:48 +0000

    implemented basic code

---
+++ cmd/kbot.go
@@ -0,0 +1,69 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"time"
+	"os"
+	"log"
+
+	"github.com/spf13/cobra"
+	bot "gopkg.in/tucnak/telebot.v2"
+
+)
+
+var (
+	BotToken = os.Getenv("BOT_TOKEN")
+)
+
+// kbotCmd represents the kbot command
+var kbotCmd = &cobra.Command{
+	Use:   "kbot",
+	Aliases: []string{"start"},
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("kbot started")
+
+		kbot, err := bot.NewBot(bot.Settings{
+			URL: "",
+			Token: BotToken,
+			Poller: &bot.LongPoller{Timeout: 10 * time.Second},
+		})
+	
+		if err != nil {
+			log.Fatalf("error", err)
+			return
+		}
+	
+		kbot.Handle(bot.OnText, func(m bot.Message) error {
+			fmt.Println(m.Text)
+
+			return err;
+		})
+	
+		kbot.Start()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(kbotCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// kbotCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// kbotCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
